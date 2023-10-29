3bc09c6ebb07b5a6afc96b9d720a01ca2683a754
Tushar Mathur <tusharmathur2411@gmail.com>
2023-10-29 21:24:43 +0530

    Portfolio home

---
+++ src/app/app.constants.ts
@@ -0,0 +1,9 @@
+export const NAME = 'Tushar Mathur';
+
+export const INTRODUCTION = `I'm a React developer with over 6 years of experience. I've had the pleasure of working on a diverse range of projects, including both B2B and B2C applications.
+\n
+My journey has been dedicated to creating seamless user interfaces and delivering exceptional user experiences. In this portfolio, you'll find a glimpse of my work, showcasing my adaptability to different project needs.
+\n
+If you're looking for an experienced developer to collaborate on your web project, I'm here to help. Let's transform your ideas into reality with the power of React.`;
+
+export const LINKEDEIN_PROFILE = 'https://www.linkedin.com/in/tushar-mathur-16b202109/';
